5c9403c47aba39cb67d5acda7e3d566d3bfa61dd
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-05-28 16:25:56 +0300

    Add util methods to SecretDataModel.dart

--- lib/SecretDataModel.dart
+++ lib/SecretDataModel.dart
@@ -4,4 +4,14 @@ class Secret {
   final name;
 
   Secret(this.id, this.secret, this.name);
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'secret': secret,
+        'name': name,
+      };
+
+  static Secret fromMap(Map<String, dynamic> map) => Secret(map['id'], map['secret'], map['name']);
+
+  String toString() => "Secret(id: $id, secret: $secret, name: $name";
 }
