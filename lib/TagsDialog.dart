23e5d7f531e4268e84cf8a1c7e6d71c03c954752
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-05-31 14:53:55 +0300

    Create dialog for editing text

--- lib/TagsDialog.dart
+++ lib/TagsDialog.dart
@@ -56,3 +56,48 @@ class _TagsDialogState extends State<TagsDialog> {
     }
   }
 }
+
+class TextEditorDialog extends StatefulWidget {
+  const TextEditorDialog({Key key}) : super(key: key);
+
+  @override
+  _TextEditorDialogState createState() => _TextEditorDialogState();
+}
+
+class _TextEditorDialogState extends State<TextEditorDialog> {
+  String tagName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: AlertDialog(
+        title: const Text("Enter new tag"),
+        content: TextField(
+          decoration: InputDecoration(
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(),
+            ),
+            hintText: 'Tag',
+          ),
+          onChanged: (string) {
+            tagName = string;
+            setState(() {});
+          },
+          onSubmitted: save,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              save(tagName);
+            },
+            child: Text("Save"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void save(String result) {
+    Navigator.pop(context, result);
+  }
+}
