88d30f31c1f20a4296f035e27c07116e74448e92
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-06-01 12:26:16 +0300

    Enter base of text fields

--- lib/dialog/EditSecretDialog.dart
+++ lib/dialog/EditSecretDialog.dart
@@ -15,6 +15,14 @@ class _EditSecretDialogState extends State<EditSecretDialog> {
 
   Secret _secret;
 
+  final _secretController = TextEditingController();
+  final _typeController = TextEditingController();
+  final _labelController = TextEditingController();
+  final _issuerController = TextEditingController();
+  final _counterOrPeriodController = TextEditingController();
+  final _digitsController = TextEditingController();
+  final _algorithmController = TextEditingController();
+
   _EditSecretDialogState(this._secret);
 
   @override
@@ -26,18 +34,97 @@ class _EditSecretDialogState extends State<EditSecretDialog> {
         child: ListView(
           children: [
             // Secret
+            TextField(
+              controller: _secretController,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                hintText: 'Enter secret',
+              ),
+              onChanged: (string) => setState(() {}),
+              // onSubmitted: // TODO: continue to next field,
+            ),
             Divider(),
             // Type
+            // TODO: make radio button instead
+            TextField(
+              controller: _typeController,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                hintText: 'Enter type',
+              ),
+              onChanged: (string) => setState(() {}),
+              // onSubmitted: // TODO: continue to next field,
+            ),
             Divider(),
             // Label
+            TextField(
+              controller: _labelController,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                hintText: 'Enter label',
+              ),
+              onChanged: (string) => setState(() {}),
+              // onSubmitted: // TODO: continue to next field,
+            ),
             Divider(),
             // Issuer
+            TextField(
+              controller: _issuerController,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                hintText: 'Enter issuer',
+              ),
+              onChanged: (string) => setState(() {}),
+              // onSubmitted: // TODO: continue to next field,
+            ),
             Divider(),
             // Counter or period (depends on type)
+            // TODO: change hint text by type
+            TextField(
+              controller: _counterOrPeriodController,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                hintText: 'Enter counter or period',
+              ),
+              onChanged: (string) => setState(() {}),
+              // onSubmitted: // TODO: continue to next field,
+            ),
             Divider(),
             // Digits
+            TextField(
+              controller: _digitsController,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                hintText: 'Enter digits',
+              ),
+              onChanged: (string) => setState(() {}),
+              // onSubmitted: // TODO: continue to next field,
+            ),
             Divider(),
             // Algorithm
+            TextField(
+              controller: _algorithmController,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+                hintText: 'Enter algorithm',
+              ),
+              onChanged: (string) => setState(() {}),
+              // onSubmitted: // TODO: close keyboard,
+            ),
           ],
         ),
       ),
