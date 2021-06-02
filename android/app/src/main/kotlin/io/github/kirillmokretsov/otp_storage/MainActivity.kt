d1e1695d972a12e10a41e01be8c53d2ce39424e3
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-06-02 17:47:12 +0300

    Prevent screenshots on android

--- android/app/src/main/kotlin/io/github/kirillmokretsov/otp_storage/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/kirillmokretsov/otp_storage/MainActivity.kt
@@ -1,6 +1,18 @@
 package io.github.kirillmokretsov.otp_storage
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import io.flutter.embedding.android.FlutterActivity
+import android.view.WindowManager.LayoutParams;
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        window.addFlags(LayoutParams.FLAG_SECURE)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(LayoutParams.FLAG_SECURE)
+    }
 }
