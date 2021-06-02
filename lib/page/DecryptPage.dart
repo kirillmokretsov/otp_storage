e3e4833c5e6aaf850427844b67477f6860ec56f0
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-06-02 15:35:00 +0300

    Create new function to check key

--- lib/page/DecryptPage.dart
+++ lib/page/DecryptPage.dart
@@ -1,9 +1,8 @@
-import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
 import 'package:otp_storage/database/Database.dart';
 import 'package:otp_storage/datamodel/SecretDataModel.dart';
 import 'package:otp_storage/page/OTPsListPage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:otp_storage/utils/Utils.dart';
 
 class DecryptPage extends StatefulWidget {
   const DecryptPage({Key key}) : super(key: key);
@@ -45,33 +44,21 @@ class _DecryptPageState extends State<DecryptPage> {
             ),
             ElevatedButton(
               onPressed: () async {
-                SharedPreferences _test = await SharedPreferences.getInstance();
-                String encryptedTest = _test.getString('encrypted_test') ?? '';
-                String decryptedTest = _test.getString('decrypted_test') ?? '';
-                if (encryptedTest == '' || decryptedTest == '') {
-                  throw Exception(
-                      'No encrypted_test or decrypted_test shared preference');
+                if (await Utils.isKeyIsRight(_key)) {
+                  List<Secret> _listOfSecrets = await DB().getSecrets();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          OTPsListPage(_listOfSecrets),
+                    ),
+                  );
                 } else {
-                  final key = encrypt.Key.fromUtf8(_key);
-                  final encrypter = encrypt.Encrypter(encrypt.AES(key));
-                  final encrypted = encrypt.Encrypted.fromBase64(encryptedTest);
-                  final decrypted = encrypter.decrypt(encrypted);
-                  if (decrypted == decryptedTest) {
-                    List<Secret> _listOfSecrets = await DB().getSecrets();
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            OTPsListPage(_listOfSecrets),
-                      ),
-                    );
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Wrong key!'),
-                      ),
-                    );
-                  }
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Wrong key!'),
+                    ),
+                  );
                 }
               },
               child: Text('Decrypt'),
--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:otp/otp.dart';
 import 'package:recase/recase.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 import '../datamodel/SecretDataModel.dart';
 import '../enum/OTPType.dart';
@@ -171,5 +173,20 @@ class Utils {
     }
 
     return SimpleIconsGetter.find(name);
+  }
+
+  static Future<bool> isKeyIsRight(String _key) async {
+    SharedPreferences _test = await SharedPreferences.getInstance();
+    String encryptedTest = _test.getString('encrypted_test') ?? '';
+    String decryptedTest = _test.getString('decrypted_test') ?? '';
+    if (encryptedTest == '' || decryptedTest == '') {
+      throw Exception(
+          'No encrypted_test or decrypted_test shared preference');
+    } else {
+      final key = encrypt.Key.fromUtf8(_key);
+      final encrypter = encrypt.Encrypter(encrypt.AES(key));
+      final encrypted = encrypt.Encrypted.fromBase64(encryptedTest);
+      final decrypted = encrypter.decrypt(encrypted);
+      return decryptedTest == decrypted;
   }
 }
